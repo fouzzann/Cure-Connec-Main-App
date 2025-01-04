ecf70c593e7b0a042aa7ce6dd8dfb7cafcaa5ec5
fouzan. p <fouzanparammal2@gmail.com>
2025-01-04 11:18:59 +0530

    added the date format on the chat screens

--- lib/views/screens/message pages/chat_screen.dart
+++ lib/views/screens/message pages/chat_screen.dart
@@ -18,8 +18,7 @@ class _ChatScreenState extends State<ChatScreen> {
   final TextEditingController _messageController = TextEditingController();
   final ChatController chatController = Get.put(ChatController());
   final FirebaseAuth _auth = FirebaseAuth.instance;
-  final ScrollController _scrollController =
-      ScrollController(); 
+  final ScrollController _scrollController = ScrollController();
 
   @override
   Widget build(BuildContext context) {
@@ -30,9 +29,7 @@ class _ChatScreenState extends State<ChatScreen> {
           IconButton(
               onPressed: () {}, icon: Icon(Icons.video_call_rounded, size: 30)),
           IconButton(onPressed: () {}, icon: Icon(Icons.call)),
-          SizedBox(
-            width: 20,
-          )
+          SizedBox(width: 20),
         ],
         backgroundColor: Colors.white,
         leading: IconButton(
@@ -82,167 +79,202 @@ class _ChatScreenState extends State<ChatScreen> {
         ),
       ),
       body: Column(
-          children: [
-            Expanded(
-              child: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
-                stream: FirebaseFirestore.instance
-                    .collection('chatRooms')
-                    .doc("${_auth.currentUser!.uid}_${widget.druid}")
-                    .collection('messages')
-                    .orderBy('timestamp')
-                    .snapshots(),
-                builder: (context, snapshot) {
-                  if (snapshot.hasError) {
-                    return Center(child: Text(snapshot.error.toString()));
-                  } else if (snapshot.connectionState ==
-                      ConnectionState.waiting) {
-                    return const Center(child: CircularProgressIndicator());
-                  }
-                  if (!snapshot.hasData) {
-                    return const Center(child: Text("No messages yet."));
-                  }
-                  final listMessages = snapshot.data!.docs;
-                  if (listMessages.isEmpty) {
-                    return const Center(child: Text("No messages yet."));
-                  }
+        children: [
+          Expanded(
+            child: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+              stream: FirebaseFirestore.instance
+                  .collection('chatRooms')
+                  .doc("${_auth.currentUser!.uid}_${widget.druid}")
+                  .collection('messages')
+                  .orderBy('timestamp')
+                  .snapshots(),
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return Center(child: Text(snapshot.error.toString()));
+                } else if (snapshot.connectionState ==
+                    ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                }
+                if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+                  return const Center(child: Text("No messages yet."));
+                }
 
-                  // Scroll to the bottom whenever new data comes in
-                  Future.delayed(Duration.zero, () {
-                    if (_scrollController.hasClients) {
-                      _scrollController.jumpTo(_scrollController
-                          .position.maxScrollExtent); // Scroll to bottom
+                final listMessages = snapshot.data!.docs;
+
+                // Scroll to the bottom whenever new data comes in
+                Future.delayed(Duration.zero, () {
+                  if (_scrollController.hasClients) {
+                    _scrollController
+                        .jumpTo(_scrollController.position.maxScrollExtent);
+                  }
+                });
+
+// Inside your ListView.builder
+                return ListView.builder(
+                  controller: _scrollController,
+                  itemCount: listMessages.length,
+                  itemBuilder: (context, index) {
+                    final message = listMessages[index];
+
+                    // Safely handle null timestamps
+                    String formattedDate = '';
+                    String formattedTime = '';
+                    DateTime? timestamp;
+                    if (message['timestamp'] != null) {
+                      timestamp = (message['timestamp'] as Timestamp).toDate();
+                      formattedDate =
+                          DateFormat('MMMM dd, yyyy').format(timestamp);
+                      formattedTime = DateFormat('hh:mm a').format(timestamp);
                     }
-                  });
 
-                  return ListView.builder(
-                    controller:
-                        _scrollController, // 2. Attach the ScrollController
-                    itemCount: listMessages.length,
-                    itemBuilder: (context, index) {
-                      final message = listMessages[index];
-
-                      // Format the timestamp if it's not null
-                      String formattedTime = '';
-                      if (message['timestamp'] != null) {
-                        var timestamp = (message['timestamp'] as Timestamp)
-                            .toDate(); // Convert Firestore timestamp to DateTime
-                        formattedTime = DateFormat('hh:mm a').format(
-                            timestamp); // Format time as 12-hour format (e.g., 10:30 AM)
+                    // Check if the date is different from the previous message
+                    bool showDateHeader = true;
+                    if (index > 0) {
+                      final previousMessage = listMessages[index - 1];
+                      if (previousMessage['timestamp'] != null) {
+                        var prevTimestamp =
+                            (previousMessage['timestamp'] as Timestamp)
+                                .toDate();
+                        showDateHeader =
+                            DateFormat('yyyy-MM-dd').format(prevTimestamp) !=
+                                DateFormat('yyyy-MM-dd')
+                                    .format(timestamp ?? DateTime.now());
                       }
+                    }
 
-                      return Align(
-                        alignment: message['senderId'] != _auth.currentUser!.uid
-                            ? Alignment.centerLeft
-                            : Alignment.centerRight,
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Column(
-                            crossAxisAlignment:
-                                message['senderId'] != _auth.currentUser!.uid
-                                    ? CrossAxisAlignment.start
-                                    : CrossAxisAlignment.end,
-                            children: [
-                              Container(
-                                padding: const EdgeInsets.symmetric(
-                                    horizontal: 16.0, vertical: 10.0),
-                                decoration: BoxDecoration(
-                                  color: message['senderId'] !=
-                                          _auth.currentUser!.uid
-                                      ? Colors.grey[200]
-                                      : Colors.blue[100],
-                                  borderRadius: BorderRadius.circular(20),
-                                ),
-                                child: Column(
-                                  crossAxisAlignment: message['senderId'] !=
-                                          _auth.currentUser!.uid
-                                      ? CrossAxisAlignment.start
-                                      : CrossAxisAlignment.end,
-                                  children: [
-                                    Text(
-                                      message['message'],
-                                      style: const TextStyle(fontSize: 16),
-                                    ),
-                                    // Display the time inside the container
-                                    Padding(
-                                      padding: const EdgeInsets.only(top: 4.0),
-                                      child: Text(
-                                        formattedTime,
-                                        style: const TextStyle(
-                                          fontSize: 12,
-                                          color: Colors.grey,
-                                        ),
-                                      ),
-                                    ),
-                                  ],
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        if (showDateHeader && timestamp != null)
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 8.0),
+                            child: Center(
+                              child: Text(
+                                formattedDate,
+                                style: const TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.grey,
                                 ),
                               ),
-                            ],
+                            ),
+                          ),
+                        Align(
+                          alignment:
+                              message['senderId'] != _auth.currentUser!.uid
+                                  ? Alignment.centerLeft
+                                  : Alignment.centerRight,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              crossAxisAlignment:
+                                  message['senderId'] != _auth.currentUser!.uid
+                                      ? CrossAxisAlignment.start
+                                      : CrossAxisAlignment.end,
+                              children: [
+                                Container(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 16.0, vertical: 10.0),
+                                  decoration: BoxDecoration(
+                                    color: message['senderId'] !=
+                                            _auth.currentUser!.uid
+                                        ? Colors.grey[200]
+                                        : Colors.blue[100],
+                                    borderRadius: BorderRadius.circular(20),
+                                  ),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        message['message'] ?? '',
+                                        style: const TextStyle(fontSize: 16),
+                                      ),
+                                      if (timestamp != null)
+                                        Padding(
+                                          padding:
+                                              const EdgeInsets.only(top: 4.0),
+                                          child: Text(
+                                            formattedTime,
+                                            style: const TextStyle(
+                                              fontSize: 12,
+                                              color: Colors.grey,
+                                            ),
+                                          ),
+                                        ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
                         ),
-                      );
-                    },
-                  );
-                },
-              ),
+                      ],
+                    );
+                  },
+                );
+              },
             ),
-            Padding(
-              padding: const EdgeInsets.all(18.0),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                      controller: _messageController,
-                      decoration: InputDecoration(
-                        labelText: "Type Message...",
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30),
-                        ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(18.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _messageController,
+                    decoration: InputDecoration(
+                      labelText: "Type Message...",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30),
                       ),
-                      textCapitalization: TextCapitalization.sentences,
                     ),
+                    textCapitalization: TextCapitalization.sentences,
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      height: 50,
-                      width: 50,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Colors.blue[600],
-                      ),
-                      child: Center(
-                        child: IconButton(
-                          onPressed: () async {
-                            if (_messageController.text.isNotEmpty) {
-                              final String chatRoomId =
-                                  await chatController.createChat(
-                                      widget.druid, _auth.currentUser!.uid);
-                              final chatModel = ChatModel(
-                                senderId: _auth.currentUser!.uid,
-                                resiverId: widget.druid,
-                                message: _messageController.text,
-                                timestamp: null,
-                              );
-                              chatController.sendMessage(chatModel, chatRoomId);
-                              _messageController.clear();
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.blue[600],
+                    ),
+                    child: Center(
+                      child: IconButton(
+                        onPressed: () async {
+                          String messageText =
+                              _messageController.text.trim(); // Trim spaces
+                          if (messageText.isNotEmpty) {
+                            final String chatRoomId =
+                                await chatController.createChat(
+                                    widget.druid, _auth.currentUser!.uid);
+                            final chatModel = ChatModel(
+                              senderId: _auth.currentUser!.uid,
+                              resiverId: widget.druid,
+                              message: messageText, // Use the trimmed text
+                              timestamp: Timestamp.now(),
+                            );
+                            chatController.sendMessage(chatModel, chatRoomId);
+                            _messageController.clear();
 
-                              if (_scrollController.hasClients) {
-                                _scrollController.jumpTo(
-                                    _scrollController.position.maxScrollExtent);
-                              }
+                            if (_scrollController.hasClients) {
+                              _scrollController.jumpTo(
+                                  _scrollController.position.maxScrollExtent);
                             }
-                          },
-                          icon: const Icon(Icons.send, color: Colors.white),
-                        ),
+                          }
+                        },
+                        icon: const Icon(Icons.send, color: Colors.white),
                       ),
                     ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-          ],
-        ),
-      
+          ),
+        ],
+      ),
     );
   }
 }
